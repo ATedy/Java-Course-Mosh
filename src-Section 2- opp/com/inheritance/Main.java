f6e80035622a242777afcf4bb56c22f13f891274
amanuel <amanuel.tedros@capgemini.com>
2022-02-02 13:05:04 +0000

    constructors and inheritance

--- src-Section 2- opp/com/inheritance/Main.java
+++ src-Section 2- opp/com/inheritance/Main.java
@@ -4,9 +4,8 @@ import com.oop.sec1.TextBox;
 
 public class Main {
     public static void main(String[] args) {
-        var control = new TextBox();
-        control.disabled();
-        System.out.println(control.isEnabled());
+        var box1 = new TextBox();
+
 
     }
 }
--- src-Section 2- opp/com/oop/sec1/TextBox.java
+++ src-Section 2- opp/com/oop/sec1/TextBox.java
@@ -5,6 +5,11 @@ import com.inheritance.UIControl;
 public class TextBox extends UIControl {
   private String text = "";
 
+  public TextBox() {
+    super(true);
+    System.out.println("TextBox");
+  }
+
   public void setText(String text) {
     this.text = text;
   }
