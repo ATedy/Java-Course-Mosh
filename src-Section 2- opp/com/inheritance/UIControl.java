e2680895a9395fdf602706a9c8a415a7ca664ae8
amanuel <amanuel.tedros@capgemini.com>
2022-02-03 16:29:32 +0000

    Final classes and methods

--- src-Section 2- opp/com/inheritance/UIControl.java
+++ src-Section 2- opp/com/inheritance/UIControl.java
@@ -9,6 +9,7 @@ public abstract class  UIControl {
 //        System.out.println("UIControl");
 //    }
 
+// final class where we can't inherit (extend) from them and methods where we can't override them
 
     // Abstract methods doesn't have implementation
     public abstract void render();
