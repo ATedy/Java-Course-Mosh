df2dd92228bf882e96b7d63014378676e8738ded
amanuel <amanuel.tedros@capgemini.com>
2022-02-03 12:16:05 +0000

    down and up casting

--- src-Section 2- opp/com/inheritance/Main.java
+++ src-Section 2- opp/com/inheritance/Main.java
@@ -4,10 +4,15 @@ import com.oop.sec1.TextBox;
 
 public class Main {
     public static void main(String[] args) {
+        var control = new UIControl(true);
         var textBox = new TextBox();
-        textBox.setText("Hello World");
-        System.out.println(textBox);
-
-
+        show(control);
+    }
+    private static void show(UIControl control) {
+        if(control instanceof TextBox){
+            var textBox = (TextBox) control;
+            textBox.setText("Hello World");
+        }
+        System.out.println(control);
     }
 }
