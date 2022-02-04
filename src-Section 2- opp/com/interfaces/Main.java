aec92b145a350aadf52f580e1bea89f75fd38a09
amanuel <amanuel.tedros@capgemini.com>
2022-02-04 11:33:47 +0000

    dependency injection with setters

--- src-Section 2- opp/com/interfaces/Main.java
+++ src-Section 2- opp/com/interfaces/Main.java
@@ -2,10 +2,13 @@ package com.interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        // now we creating instance of the class and inject/pass the calculator
-        // here we can create new Tax classes and modify them, our tax report won't be affect as it doesn't know about it
+
         var calculator = new TaxCalculator2018(100_000);
         var report = new TaxReport(calculator);
+        report.show();
+// using the setter we pasiing a new year tax/ class and with the report object will have dfft report displayed
+        report.setCalculator(new TaxCalculator2019());
+        report.show();
 
     }
 }
--- src-Section 2- opp/com/interfaces/TaxReport.java
+++ src-Section 2- opp/com/interfaces/TaxReport.java
@@ -1,15 +1,19 @@
 package com.interfaces;
 
 public class TaxReport {
-    // dependency injection with constructor
+    // dependency injection with setters
     private TaxCalculator calculator;
 
     public TaxReport(TaxCalculator calculator) {
       this.calculator = calculator;
     }
 
-  public void show() {
-        var tax = calculator.calculateTax();
-      System.out.println(tax);
-  }
+      public void show() {
+            var tax = calculator.calculateTax();
+          System.out.println(tax);
+      }
+
+    public void setCalculator(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
 }
