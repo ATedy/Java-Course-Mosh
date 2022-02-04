b0f119b9aa6b47af850a034ff9cb7fff8d0ac2e1
amanuel <amanuel.tedros@capgemini.com>
2022-02-04 13:51:28 +0000

    dependency injection with methods

--- src-Section 2- opp/com/interfaces/Main.java
+++ src-Section 2- opp/com/interfaces/Main.java
@@ -2,13 +2,10 @@ package com.interfaces;
 
 public class Main {
     public static void main(String[] args) {
-
         var calculator = new TaxCalculator2018(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
-// using the setter we pasiing a new year tax/ class and with the report object will have dfft report displayed
-        report.setCalculator(new TaxCalculator2019());
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
+        report.show(new TaxCalculator2019());
 
     }
 }
--- src-Section 2- opp/com/interfaces/TaxCalculator.java
+++ src-Section 2- opp/com/interfaces/TaxCalculator.java
@@ -1,7 +1,5 @@
 package com.interfaces;
 
 public interface TaxCalculator {
-    // no state and data only method declaration
-    // public here is not needed all methods here all for all classes to be implemented
      double calculateTax();
 }
