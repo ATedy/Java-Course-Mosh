f43e317d4ef48139b38644f9c5e0b1113b948450
amanuel <amanuel.tedros@capgemini.com>
2022-02-07 10:39:18 +0000

    Bad usage of interfaces

--- src-Section 2- opp/com/interfaces/TaxCalculator.java
+++ src-Section 2- opp/com/interfaces/TaxCalculator.java
@@ -1,5 +1,15 @@
 package com.interfaces;
 
 public interface TaxCalculator {
+
+
+     /**
+      -  interface for what not how
+      -  Avoid
+       1. fields
+       2.  Static methods
+       3. Private methods
+
+      */
      double calculateTax();
 }
