183bde4d2c71e4e869b2f5e0d4ec00cb53320798
amanuel <amanuel.tedros@capgemini.com>
2022-02-08 11:02:12 +0000

    multiple exceptions handling

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -2,14 +2,18 @@ package src.com.codewithmosh.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
+        // when err occurs only one of the catch blocks will execute
         try {
             var reader = new FileReader("file.txt");
-            System.out.println("File Opened");
+            var value = reader.read();
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
+        }catch (IOException e){
+            System.out.println("Couldn't read data");
         }
 
 
