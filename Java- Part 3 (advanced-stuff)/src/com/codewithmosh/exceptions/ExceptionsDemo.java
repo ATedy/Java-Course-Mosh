3af28ba335e0e25480472190555680f5d6dbe034
amanuel <amanuel.tedros@capgemini.com>
2022-02-08 10:34:17 +0000

    Java gives error unhandled exception, this example of checked exception

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -1,9 +1,12 @@
 package src.com.codewithmosh.exceptions;
 
+import java.util.logging.FileHandler;
+
 public class ExceptionsDemo {
     public static void show() {
         sayHello(null);
     }
+    var reader = new FileHandler("file.txt");
 
     public static void sayHello(String name) {
         System.out.println(name.toLowerCase());
