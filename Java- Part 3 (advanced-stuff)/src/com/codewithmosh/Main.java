dc5b78a3e57baca578c67f25303b8e608ee3a314
amanuel <amanuel.tedros@capgemini.com>
2022-02-08 11:45:32 +0000

    Rethrowing error and passing it to the caller class and catch it in there

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -4,6 +4,12 @@ import src.com.codewithmosh.exceptions.ExceptionsDemo;
 
 public class Main {
     public static void main(String[] args) {
-        ExceptionsDemo.show();
+        try {
+            ExceptionsDemo.show();
+        }
+        // Throwable is a generic exception handler Throwable as it is parent of all exc
+        catch (Throwable e) {
+            System.out.println("An unexpected error occurred");
+        }
     }
 }
