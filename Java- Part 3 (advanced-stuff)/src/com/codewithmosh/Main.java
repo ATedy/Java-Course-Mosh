dd7ac1f371ab08c18608ef11432d9431e75c42b3
amanuel <amanuel.tedros@capgemini.com>
2022-02-09 17:12:53 +0000

    Wildcard with extends and super for reading and writing respectively

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -3,13 +3,14 @@ package src.com.codewithmosh;
 import src.com.codewithmosh.generics.GenericList;
 import src.com.codewithmosh.generics.Instructor;
 import src.com.codewithmosh.generics.User;
-import src.com.codewithmosh.generics.Utlis;
+import src.com.codewithmosh.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
+        // here we can pass a list of anything to the users bc we using the wildcards
     var instructors = new GenericList<Instructor>();
     var users = new GenericList<User>();
-        Utlis.printUser(users);
+        Utils.printUsers(new GenericList<User>());
 
     }
 }
---
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/generics/Utils.java
@@ -0,0 +1,27 @@
+package src.com.codewithmosh.generics;
+
+public class Utils {
+    public static <T> void max(T first, T second){
+        return ;
+    }
+
+    public static void printUser (User user) {
+        System.out.println(user);
+
+    }
+    /**
+     * wildcards ? unknown types
+
+     - class CAP#1 extends User {}- class
+     - class Instructor extends User {} -class
+     - with extend keyword we could only read not write
+     with use super for writing
+     - but using the super kw we can only  write or store it in super class which is Object
+     */
+    public static void printUsers
+    (GenericList<? super User> users){
+        GenericList<Object> temp = new GenericList<>();
+        Object x = users.get(0);
+
+    }
+}
