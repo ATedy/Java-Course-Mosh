dfa59bfbbdc1cf3bf7e66c84304a6e865469fb00
amanuel <amanuel.tedros@capgemini.com>
2022-02-09 10:07:01 +0000

    BOxing and unboxing process for primitive values

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -4,10 +4,11 @@ import src.com.codewithmosh.generics.GenericList;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<Integer>();
-        list.add(1);
-        // no explicit cast needed
-        int number = list.get(0);
+        // Generics n primitive datatypes
+        GenericList<Integer> numbers = new GenericList<>();
+        numbers.add(1); // Boxing- tho 1 is int(primitive it will be wrapped by the Integer)
+        int number = numbers.get(0); // Unboxing - Javac will extract the number from the Integer object
+
 
     }
 }
