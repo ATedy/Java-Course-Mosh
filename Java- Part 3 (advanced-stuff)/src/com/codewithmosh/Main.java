4f371c20c72658e0f9864fd1a356246dfb9023c2
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-16 10:57:53 +0000

    Implementing iterable interface for lists

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -4,11 +4,14 @@ import src.com.codewithmosh.generics.GenericList;
 public class Main {
     public static void main(String[] args) {
     var list = new GenericList<String>();
-    var iterator = list.iterator();
-    while (iterator.hasNext()){
-        var current = iterator.next();
-        System.out.println(current);
-    }
+    list.add("a");
+    list.add("b");
+
+        for (var item : list) {
+            System.out.println(item);
+
+        }
+
 
     }
 }
--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/generics/GenericList.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/generics/GenericList.java
@@ -1,23 +1,26 @@
 package src.com.codewithmosh.generics;
 
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class GenericList<T> implements Iterable<T> {
-    private T[] items = (T[])new Object[10];
-    private int count;
 
-    public void add(T item){
-        items[count++] = item;
-    }
+  private T[] items = (T[]) new Object[10];
+  private int count;
 
-    public T get(int index){
-        return items[index];
-    }
+  public void add(T item) {
+    items[count++] = item;
+  }
 
-    @Override
-    public Iterator <T> iterator() {
-        return null;
-    }
+  public T get(int index) {
+    return items[index];
+  }
 
 
- }
+  @Override
+  public Iterator<T> iterator() {
+    return null;
+  }
+}
+
+}
