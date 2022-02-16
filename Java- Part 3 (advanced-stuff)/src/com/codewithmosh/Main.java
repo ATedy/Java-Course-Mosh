2173d65af24e9fab2b9423464736d52f70e951c9
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-16 10:09:49 +0000

    Iterable interface

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -1,16 +1,14 @@
 package src.com.codewithmosh;
 
 import src.com.codewithmosh.generics.GenericList;
-import src.com.codewithmosh.generics.Instructor;
-import src.com.codewithmosh.generics.User;
-import src.com.codewithmosh.generics.Utils;
-
 public class Main {
     public static void main(String[] args) {
-        // here we can pass a list of anything to the users bc we using the wildcards
-    var instructors = new GenericList<Instructor>();
-    var users = new GenericList<User>();
-        Utils.printUsers(new GenericList<User>());
+    var list = new GenericList<String>();
+    var iterator = list.iterator();
+    while (iterator.hasNext()){
+        var current = iterator.next();
+        System.out.println(current);
+    }
 
     }
 }
--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/generics/GenericList.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/generics/GenericList.java
@@ -1,6 +1,8 @@
 package src.com.codewithmosh.generics;
 
-public class GenericList<T> {
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T> {
     private T[] items = (T[])new Object[10];
     private int count;
 
@@ -11,4 +13,11 @@ public class GenericList<T> {
     public T get(int index){
         return items[index];
     }
-}
+
+    @Override
+    public Iterator <T> iterator() {
+        return null;
+    }
+
+
+ }
