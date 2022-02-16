ad08afbdb9194a5e54675f33bc891eca93c98f23
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-16 15:27:31 +0000

    Different sorts of operations collections provides

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/collections/collectionsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/collections/collectionsDemo.java
@@ -8,11 +8,13 @@ public class collectionsDemo {
   public static void show(){
     Collection<String> collection = new ArrayList<>();
     Collections.addAll(collection, "a", "b", "c");
+    Collection<String> other = new ArrayList<>();
+    other.addAll(collection);
 
-    for (var item: collection) {
-      System.out.println(item);
+    System.out.println(collection == other);
+    System.out.println(collection.equals(other));
 
-    }
+
 
   }
 
