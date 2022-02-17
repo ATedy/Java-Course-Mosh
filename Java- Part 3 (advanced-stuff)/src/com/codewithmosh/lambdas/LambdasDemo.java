22b921a0989b3aa0eed785c990a3ffb45438f512
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-17 12:25:50 +0000

    Functional Interface Java

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -1,15 +1,12 @@
 package src.com.codewithmosh.lambdas;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 
-/**
- * This supplier has specialization with primitive values and we have to call it using the get()
- */
 public class LambdasDemo {
   public static void show() {
-    Supplier<Double> getRandom = () -> Math.random();
-    var random = getRandom.get();
-    System.out.println(random);
+    Function<String, Integer> map = str -> str.length();
+    var length = map.apply("Sky");
+    System.out.println(length);
 
   }
 
