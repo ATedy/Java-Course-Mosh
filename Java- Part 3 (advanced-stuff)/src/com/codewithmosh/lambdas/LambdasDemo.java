b653d3adf94d93f09b5b8d58239fe66368d70aaf
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-17 09:36:43 +0000

    anonymous class implementation

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -1,8 +1,19 @@
 package src.com.codewithmosh.lambdas;
 
+/**
+ * new Printer Interface is anonymous inner class because
+ - no name and its inside a class
+ */
 public class LambdasDemo {
   public static void show() {
-    greet(new ConsolePrinter());
+
+    greet(new Printer() {
+      @Override
+      public void print(String message) {
+        System.out.println(message);
+
+      }
+    });
 
   }
   public  static void greet(Printer printer) {
