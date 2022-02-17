6026534a8d75771fbb8edc95430148953a63c52a
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-17 12:39:46 +0000

    Predicate interfaces

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -1,24 +1,12 @@
 package src.com.codewithmosh.lambdas;
 
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class LambdasDemo {
   public static void show() {
-    // "key:value"
-    // first: "key=value"
-    // second: "{key=value}"
-    Function<String, String> replaceColon = str -> str.replace(":", "=");
-    Function<String, String> addBraces = str -> "{" + str + "}";
+    Predicate<String> isLongerThan5 = str -> str.length() > 5;
+    var result = isLongerThan5.test("sky");
 
-    // Declarative programming
-    var result = replaceColon
-        .andThen(addBraces)
-        .apply("key:value");
-
-    // same result in reverse order
-    var result1 =  addBraces.compose(replaceColon).apply("key:value");
-    System.out.println(result);
-    System.out.println(result1);
 
 
   }
