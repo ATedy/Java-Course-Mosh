4cc8fafc6786cd1591db41d23f53bf1edd60d85f
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-17 12:36:41 +0000

    Composing functions

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -4,11 +4,23 @@ import java.util.function.Function;
 
 public class LambdasDemo {
   public static void show() {
-    Function<String, Integer> map = str -> str.length();
-    var length = map.apply("Sky");
-    System.out.println(length);
+    // "key:value"
+    // first: "key=value"
+    // second: "{key=value}"
+    Function<String, String> replaceColon = str -> str.replace(":", "=");
+    Function<String, String> addBraces = str -> "{" + str + "}";
+
+    // Declarative programming
+    var result = replaceColon
+        .andThen(addBraces)
+        .apply("key:value");
+
+    // same result in reverse order
+    var result1 =  addBraces.compose(replaceColon).apply("key:value");
+    System.out.println(result);
+    System.out.println(result1);
+
 
   }
-
 
 }
