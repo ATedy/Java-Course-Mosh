c1c5765f90fdca289789b664f348000a2e3dd3b2
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-17 12:45:50 +0000

    Combining Predicates

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -2,12 +2,17 @@ package src.com.codewithmosh.lambdas;
 
 import java.util.function.Predicate;
 
+
+/** Predicate
+  - logical and, or, negate
+ */
 public class LambdasDemo {
   public static void show() {
-    Predicate<String> isLongerThan5 = str -> str.length() > 5;
-    var result = isLongerThan5.test("sky");
+    Predicate<String> hasLeftBrace = str -> str.startsWith("{");
+    Predicate<String> hasRightBrace = str -> str.endsWith("}");
 
-
+    Predicate<String> hasLeftAndRightBraces= hasLeftBrace.and(hasRightBrace);
+    hasLeftAndRightBraces.test("{key:value}");
 
   }
 
