aa762e1d15dcf93e26c3e09a85457e5531d16bae
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-17 10:26:50 +0000

    Method references with instances of classes

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -4,11 +4,12 @@ package src.com.codewithmosh.lambdas;
  * Method references
  */
 public class LambdasDemo {
-  public static void print(String message) {}
+  public  void print(String message) {}
 
   public  void show() {
-    greet(message -> print(message));
-    greet(LambdasDemo::print);
+    var demo = new LambdasDemo();
+    greet(message -> demo.print(message));
+    greet(demo::print);
 
   }
 
