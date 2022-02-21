c87fbe485fd5a176d1e3bed85f4404439b5b391b
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 17:25:45 +0000

    Partitioning elements

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
@@ -10,16 +10,13 @@ public class StreamsDemo {
         new Movie("b", 20, Genre.ACTION),
         new Movie("c", 30, Genre.ACTION)
     );
-
-var result = movies.stream()
-    .collect(Collectors.groupingBy(
-        Movie::getGenre,
-        Collectors.mapping(
-            Movie::getTitle,
-            Collectors.joining(", "))));
+   var result =  movies.stream()
+        .collect(Collectors.partitioningBy(
+            m -> m.getLikes() > 20,
+            Collectors.mapping(Movie::getTitle,
+                Collectors.joining(", "))));
 
     System.out.println(result);
-
   }
 }
 
