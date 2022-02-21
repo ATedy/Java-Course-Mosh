9a6455752a91366cf00330e01bea945dbcca829a
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 10:10:59 +0000

    Declarative and Imperative examples

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/Movie.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/Movie.java
@@ -1,30 +1,19 @@
-public class Movie implements Comparable<Movie> {
+package src.com.codewithmosh.streams;
+
+public class Movie  {
   private String title;
   private int likes;
 
-
-  private Genre genre;
-
-  public Movie(String title, int likes, Genre genre) {
+  public Movie(String title, int likes) {
     this.title = title;
     this.likes = likes;
-    this.genre = genre;
   }
 
   public String getTitle() {
     return title;
   }
 
-  public Genre getGenre() {
-    return genre;
-  }
-
   public int getLikes() {
     return likes;
-  }
-
-  @Override
-  public int compareTo(Movie o) {
-    return 0;
   }
 }
--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
@@ -1,4 +1,29 @@
+package src.com.codewithmosh.streams;
+
+import java.util.List;
+
 public class StreamsDemo {
   public static void show() {
+    List<Movie> movies = List.of(
+        new Movie("a", 10),
+        new Movie("b", 15),
+        new Movie("c", 20)
+    );
+// imperative programming
+    int count = 0;
+    for(var movie: movies){
+      if (movie.getLikes() > 10)
+        count++;
+    }
+   // Declarative (functional) Programming
+    var count2 = movies.stream()
+        .filter(movie -> movie.getLikes() > 10)
+        .count();
   }
 }
+
+/**
+ * Imperative Programming (expressing what needs to be done)
+ * Declarative: how needs to be done
+ *  Stream: is a sequence of objects, doesn't store data like a collection but its a way to get data out a collection
+ */
