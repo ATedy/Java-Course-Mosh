24df078da49c2b908bd8e07ef2b05cd9166e7f8e
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 16:58:31 +0000

    general purpose reduction with reduce method

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
@@ -1,6 +1,7 @@
 package src.com.codewithmosh.streams;
 
 import java.util.List;
+import java.util.Optional;
 
 public class StreamsDemo {
   public static void show() {
@@ -9,9 +10,26 @@ public class StreamsDemo {
         new Movie("b", 20),
         new Movie("c", 30)
     );
-    // anyMatch, allMatch, noneMatch
-    movies.stream()
-        .noneMatch(m ->m.getLikes() > 20);
+    /**
+     *   optional class represents an object that may or may not have a value
+     *      how reducer works(it will accumulate the values
+     *      [10, 20, 30]
+     *      [30, 30]
+     *      [60]
+     *  so if we don't have some ) will be outputted
+     */
+
+   Optional<Integer> sum =  movies.stream()
+        .map(movie -> movie.getLikes())
+        .reduce(Integer::sum);
+
+   // general purpose reduction with reduce method
+    Integer sum1 =  movies.stream()
+        .map(movie -> movie.getLikes())
+        .reduce(0, Integer::sum);
+
+    System.out.println(sum.orElse(0));
+    System.out.println(sum1);
 
   }
 }
