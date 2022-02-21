6c11318c0cac58ce4015b628f23cc259fd2305ed
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 10:26:53 +0000

    Creating and using streams

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -1,11 +1,11 @@
 package src.com.codewithmosh;
 
-import src.com.codewithmosh.lambdas.LambdasDemo;
+import src.com.codewithmosh.streams.StreamsDemo;
 
 public class Main {
 
     public static void main(String[] args) {
-        LambdasDemo.show();
+        StreamsDemo.show();
 
     }
 }
