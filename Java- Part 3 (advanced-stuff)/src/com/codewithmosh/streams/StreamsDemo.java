0bf49ed21eda0aac675f4c7a6d34274008aeed31
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 10:44:46 +0000

    Intermediate and Terminal streams

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
@@ -1,16 +1,27 @@
 package src.com.codewithmosh.streams;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 public class StreamsDemo {
   public static void show() {
-    // Stream<List<x>> -> Stream<x>
-   var stream =  Stream.of(List.of(1,2,3), List.of(4,5,6));
-   stream
-       .flatMap(list -> list.stream())
-       .forEach(n -> System.out.println(n));
+    List<Movie> movies = List.of(
+        new Movie("a", 10),
+        new Movie("b", 15),
+        new Movie("c", 20)
+    );
 
+    // Intermediate : map, filter
+    // Terminal: forEach
+     movies.stream()
+        .filter(movie -> movie.getLikes() >10)
+        .forEach(m -> System.out.println(m));
+
+// or
+    Predicate<Movie> isPopular = m -> m.getLikes() >10;
+    movies.stream()
+        .filter(isPopular)
+        .forEach(m -> System.out.println(m));
 
 
   }
