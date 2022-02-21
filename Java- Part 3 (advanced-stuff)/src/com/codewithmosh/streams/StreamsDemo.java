2a52310210f7dcb7d88688c3e4bda6670ea49391
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 15:55:25 +0000

    distinct stream

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
@@ -1,26 +1,20 @@
 package src.com.codewithmosh.streams;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class StreamsDemo {
   public static void show() {
     List<Movie> movies = List.of(
-        new Movie("b", 15),
+        new Movie("b", 10),
         new Movie("a", 10),
-        new Movie("c", 20)
+        new Movie("c", 20),
+        new Movie("d", 30)
     );
-
-    movies.stream()
-        .sorted((a, b) -> a.getTitle().compareTo(b.getTitle()))
-        .sorted(Comparator.comparing(Movie::getTitle).reversed())
-        .forEach(m -> System.out.println(m.getTitle()));
-
-
-    movies.stream()
-        .sorted(Comparator.comparing(movie -> movie.getTitle()))
-        .forEach(m -> System.out.println(m.getTitle()));
-
+ // distinct stream is used for getting values with no duplication
+  movies.stream()
+      .map(Movie::getLikes)
+      .distinct()
+      .forEach(System.out::println);
   }
 }
 
