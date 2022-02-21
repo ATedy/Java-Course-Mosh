3630857648d7d0823968b26ad29f8080befee883
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-21 10:33:53 +0000

    creating a single big list using flatmap

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/streams/StreamsDemo.java
@@ -1,17 +1,16 @@
 package src.com.codewithmosh.streams;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
   public static void show() {
-    List<Movie> movies = List.of(
-        new Movie("a", 10),
-        new Movie("b", 15),
-        new Movie("c", 20)
-    );
-    movies.stream()
-        .map(movie -> movie.getTitle())
-        .forEach(name -> System.out.println(name));
+   var stream =  Stream.of(List.of(1,2,3), List.of(4,5,6));
+   stream
+       .flatMap(list -> list.stream())
+       .forEach(n -> System.out.println(n));
+
+
 
   }
 }
