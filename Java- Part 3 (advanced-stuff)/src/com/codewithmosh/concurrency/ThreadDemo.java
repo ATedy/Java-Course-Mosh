273058f0fb145cc92cf994cd0540b4cf3cdefffd
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-22 14:48:39 +0000

    Concurrency Issues

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/ThreadDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/ThreadDemo.java
@@ -8,8 +8,6 @@ public class ThreadDemo {
     Thread.sleep(1000);
     System.out.println("file is ready to be scanned");
 
-    // this interrupt doesn't actually interrupt the thread, it sends an interrupt message instead and we check the
-    // request and handle it
     thread.interrupt();
 
   }
