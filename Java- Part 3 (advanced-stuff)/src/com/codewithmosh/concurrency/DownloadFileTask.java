3dc0041a37b7e9b829f810f6d86af33b33b36d9d
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-22 15:30:30 +0000

    confinement Example

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/DownloadFileTask.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/DownloadFileTask.java
@@ -4,18 +4,21 @@ public class DownloadFileTask implements Runnable{
 
   private DownloadStatus status;
 
-  public DownloadFileTask(DownloadStatus status) {
-    this.status = status;
+  public DownloadFileTask() {
+    this.status = new DownloadStatus();
   }
 
   @Override
   public void run() {
-    System.out.println("downloading a file: " + Thread.currentThread().getName());
 
     for (int  i = 0; i < 10_000; i++) {
       if (Thread.currentThread().isInterrupted())return;
       status.incrementTotalBytes();
     }
     System.out.println("Download complete: " + Thread.currentThread().getName());
+  }
+
+  public DownloadStatus getStatus() {
+    return status;
   }
 }
--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/ThreadDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/ThreadDemo.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 public class ThreadDemo {
   public static void show() throws InterruptedException {
-    var status = new DownloadStatus();
 
     List<Thread> threads = new ArrayList<>();
+    List<DownloadFileTask> tasks = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
-      Thread thread = new Thread(new DownloadFileTask(status));
+      var task = new DownloadFileTask();
+      tasks.add(task);
+
+      Thread thread = new Thread(task);
       thread.start();
       threads.add(thread);
     }
@@ -17,8 +20,13 @@ public class ThreadDemo {
     for (var thread: threads) {
       thread.join();
     }
-    System.out.println(status.getTotalBytes());
 
+    var totalBytes = tasks.stream()
+        .map(t ->t.getStatus().getTotalBytes())
+        .reduce(Integer::sum);
+
+    // we will have 100_000 as expected as threads aren't modifying a shared object
+    System.out.println(totalBytes);
   }
 
 }
