afe19441f0f8345820b10d43a533005b011108c5
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-22 14:33:08 +0000

    Joining multiple threads

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -3,7 +3,7 @@ import src.com.codewithmosh.concurrency.ThreadDemo;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadDemo.show();
 
     }
--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/ThreadDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/concurrency/ThreadDemo.java
@@ -1,12 +1,16 @@
 package src.com.codewithmosh.concurrency;
 
 public class ThreadDemo {
-  public static void show() {
+  public static void show() throws InterruptedException {
     System.out.println(Thread.currentThread().getName());
 
-        for (int  i = 0; i < 10; i++) {
+        for (int  i = 0; i < 5; i++) {
           Thread thread = new Thread(new DownloadFileTask());
           thread.start();
+          // join block the 2nd thread from starting until thr 1st one finishes
+          thread.join(5000);
+
+          System.out.println("file is ready to be scanned");
     }
 
   }
