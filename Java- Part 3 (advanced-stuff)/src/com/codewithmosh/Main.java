c62cb7204d510f7c78c21932be69bf5818ca4bca
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-22 11:34:53 +0000

    Thread summary and example

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/Main.java
@@ -1,11 +1,15 @@
 package src.com.codewithmosh;
 
-import src.com.codewithmosh.streams.StreamsDemo;
-
 public class Main {
 
+    /**
+     -  this programme is using 2 threads,
+     -  one id main thread for running main method,
+     -  2 for garbage collector: for removing unused objects from memory
+     */
     public static void main(String[] args) {
-        StreamsDemo.show();
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
 
     }
 }
