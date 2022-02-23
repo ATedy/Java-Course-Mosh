716944376a3d188483eb7a700c091ad10fc450b6
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-23 14:37:03 +0000

    waiting for first task

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
@@ -1,28 +1,19 @@
 package src.com.codewithmosh.executors;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
 
   public static void show() {
-    var first = CompletableFuture.supplyAsync(() ->1);
-    var second = CompletableFuture.supplyAsync(() ->2);
-    var third = CompletableFuture.supplyAsync(() ->3);
+   var first =  CompletableFuture.supplyAsync(() ->{
+     LongTask.simulate();
+     return 20;
+   });
+    var second =  CompletableFuture.supplyAsync(()-> 20 );
 
-      // waiting all tasks to finish
-    // variable all void bc we don't know what the 3 Comple will return
-    var all = CompletableFuture.allOf(first, second, third);
-    all.thenRun(() -> {
-      try {
-        var firstResult = first.get();
-        System.out.println(firstResult);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      } catch (ExecutionException e) {
-        e.printStackTrace();
-      }
-      System.out.println("All tasks completed successfully");
-    });
+     CompletableFuture
+         .anyOf(first, second)
+             .thenAccept(temp -> System.out.println(temp));
+
   }
 }
