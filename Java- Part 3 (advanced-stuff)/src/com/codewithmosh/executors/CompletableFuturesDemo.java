e15912123c5ad834cbe761e65670b837cf221f0b
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-23 14:31:58 +0000

    waiting for Many tasks to complete

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
@@ -1,20 +1,28 @@
 package src.com.codewithmosh.executors;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
 
   public static void show() {
-    var first = CompletableFuture.supplyAsync(() -> "20USD")
-        .thenApply(str -> {
-          var price = str.replace("USD", "");
-          return Integer.parseInt(price);
+    var first = CompletableFuture.supplyAsync(() ->1);
+    var second = CompletableFuture.supplyAsync(() ->2);
+    var third = CompletableFuture.supplyAsync(() ->3);
+
+      // waiting all tasks to finish
+    // variable all void bc we don't know what the 3 Comple will return
+    var all = CompletableFuture.allOf(first, second, third);
+    all.thenRun(() -> {
+      try {
+        var firstResult = first.get();
+        System.out.println(firstResult);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      }
+      System.out.println("All tasks completed successfully");
     });
-
-    var second = CompletableFuture.supplyAsync(() -> 0.9);
-
-    first.thenCombine(second, (price, ex) -> price * ex)
-        .thenAccept(result -> System.out.println(result));
-
   }
 }
