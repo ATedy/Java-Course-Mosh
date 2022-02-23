4aff0f1b2bd36e03b1a7ffabfe602e730bb15f53
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-23 11:50:56 +0000

    Handling exceptions

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
@@ -1,15 +1,24 @@
 package src.com.codewithmosh.executors;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
   public static void show() {
-   var future =  CompletableFuture.supplyAsync(() -> 1);
-   future.thenRun(() ->{
-     System.out.println(Thread.currentThread().getName());
-     System.out.println("Done");
-   });
+    var future = CompletableFuture.supplyAsync(() -> {
+      System.out.println("Getting the current weather");
+      throw new IllegalArgumentException();
+    });
+
+    // giving a default value in case the remote temp doesn't exist using exceptionally
+    try {
+     var temperature =  future.exceptionally(ex ->1).get();
+      System.out.println(temperature);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
 
   }
-
 }
