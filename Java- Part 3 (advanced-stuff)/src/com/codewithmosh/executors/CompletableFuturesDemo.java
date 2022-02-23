d292a9eba8c4640aaaa9aabe2c1c93fd42779f8d
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-23 15:17:07 +0000

    Random Delay and module done

--- Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
+++ Java- Part 3 (advanced-stuff)/src/com/codewithmosh/executors/CompletableFuturesDemo.java
@@ -1,14 +1,27 @@
 package src.com.codewithmosh.executors;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class CompletableFuturesDemo {
 
   public static void show() {
+    var start = LocalTime.now();
    var service = new FlightService();
-   service.getQuotes()
+  var futures =  service.getQuotes()
        .map(future -> future.thenAccept(System.out::println))
        .collect(Collectors.toList());
+
+    CompletableFuture
+        .allOf(futures.toArray(new CompletableFuture[0]))
+        .thenRun(() ->{
+          var end = LocalTime.now();
+
+         var duration = Duration.between(start, end);
+          System.out.println("Retrived all quotes in " + duration.toMillis()+ "msec.");
+        });
 
 
     try {
